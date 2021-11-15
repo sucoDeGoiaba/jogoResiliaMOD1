2fa2304832d4b11ab00c535cd254ff44cf424d86
sucoDeGoiaba <flp.pxxs@gmail.com>
2021-11-15 13:13:00 -0300

    commitAdicionandoJs

---
+++ codigo/storylineVampiro/teste.js
@@ -0,0 +1,35 @@
+// var readingMode = true
+
+
+
+// while(readingMode){
+    
+// }
+
+// 
+// window.onload = function(){
+//     document.getElementById("submit-button").addEventListener("click", function() {
+//         var value = document.getElementById('input').value;
+    
+//         console.log(value);
+//     });
+// }
+
+// console.log()   
+
+document.getElementById("submit-button").addEventListener("click", function(){
+    var value = document.getElementById('submit-button').value;
+    
+
+    if(value){
+        console.log(value)
+
+        var resposta = prompt("Você recebe a opção de caçar ou ficar em casa, o que escolhe: \n\t1 - Caçar.\n\t2 - Não caçar.");
+
+        // console.log(typeof resposta);
+        if(resposta === "1"){
+            
+        }
+    }
+});
+// console.log(teste)
