cde4344b6a2707ec7583e0e7ae98eda9e64d97d1
sucoDeGoiaba <flp.pxxs@gmail.com>
2021-11-16 14:51:45 -0300

    outraArrumadaETerminandoAStoryline

--- codigo/storylineVampiro/escolhasVamp.js
+++ codigo/storylineVampiro/escolhasVamp.js
@@ -1,9 +1,9 @@
 function escolhasVamp1(){
     var resposta = prompt("Você recebe a opção de caçar ou ficar em casa, o que escolhe: \n\t1 - Caçar.\n\t2 - Não caçar.\n\n");
         if(resposta === "1"){
-            window.location.assign("./escolhas1/escolhendoCacar.html");
+            window.location.assign("./Cap1Escolha1.html");
         } else if(resposta === "2") {
-            window.location.assign("./escolhas1/naoEscolheuCacar.html");
+            window.location.assign("./Cap1Escolha2.html");
         } else {
             alert("Resposta invália.");
         }
@@ -13,14 +13,26 @@ function escolhasVamp1(){
 function escolhasVamp2(){
     var resposta = prompt("Você recebe a opção de mata-la ou não, o que escolhe: \n\t1 - Matar.\n\t2 - Não matar.\n\n");
         if(resposta === "1"){
-            window.location.assign("./escolhas1/escolhas2/escolhendoMatarAmulher.html");
+            window.location.assign("./Cap2Escolha1.html");
         } else if(resposta === "2") {
-            window.location.assign("./escolhas1/escolhas2/naoMatandoMulher.html");
+            window.location.assign("./Cap2Escolha2.html");
         } else {
             alert("Resposta invália.");
         }
 }
 
 function gameOver(){
-    window.location.assign("../../../gameover/gameOver.html");
+    window.location.assign("../gameOver.html");
+}
+
+function indoProCap3(){
+    window.location.assign("./Cap3.html")
+}
+
+function fim(){
+    window.location.assign("../paginaFim.html")
+}
+
+function deVoltaAoComeco(){
+    window.location.assign("./index.html");
 }
