439b0a9e80459da5f7e07d8a041dc291a874b2d7
sucoDeGoiaba <flp.pxxs@gmail.com>
2021-11-16 11:04:49 -0300

    settandoNovasEscolhasEArrumacao

--- codigo/storylineVampiro/escolhasVamp.js
+++ codigo/storylineVampiro/escolhasVamp.js
@@ -1,20 +1,4 @@
-// document.getElementById("submit-button").addEventListener("click", function(){
-//     var value = document.getElementById('submit-button').value;
-//     if(value){
-//         var resposta = prompt("Você recebe a opção de caçar ou ficar em casa, o que escolhe: \n\t1 - Caçar.\n\t2 - Não caçar.");
-
-//         if(resposta === "1"){
-//             window.location.assign("./escolhas1/escolhendoCacar.html");
-//         } else if(resposta === "2") {
-//             window.location.assign("./escolhas1/naoEscolheuCacar.html");
-//         } else {
-//             alert("Resposta invália.");
-//         }
-//     }
-// });
-
-function escolhas1(){
-    // console.log(1);
+function escolhasVamp1(){
     var resposta = prompt("Você recebe a opção de caçar ou ficar em casa, o que escolhe: \n\t1 - Caçar.\n\t2 - Não caçar.\n\n");
         if(resposta === "1"){
             window.location.assign("./escolhas1/escolhendoCacar.html");
@@ -26,13 +10,17 @@ function escolhas1(){
 }
 
 
-function escolhas2(){
-    var resposta = prompt("Você recebe a opção de caçar ou ficar em casa, o que escolhe: \n\t1 - Caçar.\n\t2 - Não caçar.\n\n");
+function escolhasVamp2(){
+    var resposta = prompt("Você recebe a opção de mata-la ou não, o que escolhe: \n\t1 - Matar.\n\t2 - Não matar.\n\n");
         if(resposta === "1"){
-            window.location.assign("./escolhas1/escolhendoCacar.html");
+            window.location.assign("./escolhas1/escolhas2/escolhendoMatarAmulher.html");
         } else if(resposta === "2") {
-            window.location.assign("./escolhas1/naoEscolheuCacar.html");
+            window.location.assign("./escolhas1/escolhas2/naoMatandoMulher.html");
         } else {
             alert("Resposta invália.");
         }
+}
+
+function gameOver(){
+    window.location.assign("../../../gameover/gameOver.html");
 }
